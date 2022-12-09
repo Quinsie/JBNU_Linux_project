d3a41bc5b3a9d49b674b3f25f73e64316941fc70
Quinsie <jyjh0501@naver.com>
2022-12-10 07:49:36 +0900

    base

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+int main()
+{
+	return 0;
+}
