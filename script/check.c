9125c0b4648913d804568f6c86fa954331428f48
Quinsie <81253154+Quinsie@users.noreply.github.com>
2022-12-12 16:32:26 +0900

    Update check.c

--- script/check.c
+++ script/check.c
@@ -5,22 +5,22 @@ void check(const int clientFd, const int lockerlen)
 	char str[MAXLINE] = "", num[5], chtemp;
 	int fd;
 	
-	fd = open("../resource/status", O_RDONLY);
+	fd = open("../resource/status", O_RDONLY); // lockerstatus filedescriptor
 	lseek(fd, 0, SEEK_SET);
 	
 	strcat(str, "\n\n** LOCKER STATUS **\n");
-	for (int i = 0; i < lockerlen; i++) {
+	for (int i = 0; i < lockerlen; i++) { // repeat till lockerlen
 		strcat(str, "LOCKER ");
 		sprintf(num, "%d", i + 1);
 		strcat(str, num);
 		
-		read(fd, &chtemp, sizeof(chtemp));
-		if (chtemp == '0') strcat(str, " : VOID\n");
+		read(fd, &chtemp, sizeof(chtemp)); // read lockerstatus file
+		if (chtemp == '0') strcat(str, " : VOID\n"); // if now index is 0 -> VOID, else -> IN USE
 		else strcat(str, " : IN USE\n");
 	}
 	strcat(str, "\n");
 	
-	write(clientFd, str, strlen(str) + 1);
+	write(clientFd, str, strlen(str) + 1); // send lockerstatus info to client
 	close(fd);
 }
 
@@ -28,7 +28,7 @@ void client_check(const int clientFd)
 {
 	char str[MAXLINE];
 	
-	readLine(clientFd, str);
+	readLine(clientFd, str); // recieve lockerstatus info
 	printf("%s", str);
 	printf("Return to menu...\n\n");
 	sleep(1);
